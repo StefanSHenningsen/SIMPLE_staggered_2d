54c941b211d336e33955ca80a807ebdad6ea306d
Stefan Sajin-Henningsen <anuk.ssh@gmail.com>
2024-02-20 10:32:02 +0100

    ake body for iterators #4 #5

--- main.py
+++ main.py
@@ -54,11 +54,12 @@ class InputAndResults():
         self.vy_res = []
         self.p_res = []
     
-def solver_driver(m, io_res: InputAndResults, bc: BoundaryConditions):
+def solver_driver(n_driver,n_integrator, io_res: InputAndResults, bc: BoundaryConditions):
     '''
     Driver program to take timestep and save results to array
     Input:
-        m: max number of times to store in result/outer timesteps to take
+        n_driver: max number of times to store in result/outer timesteps to take
+        n_integrator: max number of timesteps in integrator
         io_res: class holding Input and results
     '''
     io_res.t_res.append(bc.t0)
@@ -69,12 +70,12 @@ def solver_driver(m, io_res: InputAndResults, bc: BoundaryConditions):
     t = bc.t0
     vx, vy, p = bc.vx0, bc.vy0, bc.p0
 
-    for _ in range(m):
+    for _ in range(n_driver):
         t_end = t + io_res.dt_res
         if t_end > io_res.t_final:
             t_end = io_res.t_final
         dt = io_res.dt
-        solver_integrator(t, vx, vy, p, dt, t_end)
+        solver_integrator(t, vx, vy, p, dt, t_end, n_integrator)
 
         io_res.t_res.append(t)
         io_res.vx_res.append(np.copy(vx))
@@ -85,23 +86,78 @@ def solver_driver(m, io_res: InputAndResults, bc: BoundaryConditions):
             break
 
 
-def solver_integrator(t, vx, vy, p, dt, t_end):
+def solver_integrator(t, vx, vy, p, dt, t_end, n_integrator):
     '''
     Take one output step
     '''
-    
+    for _ in range(n_integrator):
+        if (t_end - t < dt): 
+            dt = t_end - t
+        solver_propagator(t, vx, vy, p, dt)
+        if t > t_end:
+            return None
+    raise RuntimeError("n_integrator was reached before it finished")
+
+def solver_propagator(t, vx, vy, p, dt):
+    '''
+    Take one single timestep p. 188
+    '''
+    #loop in outer iterations
+    solver_outer_iteration(t, vx, vy, p, dt)
+    #go back and do it again + check for convergence
+
+
+
+
     return 0
 
-def solver_propagator():
+def solver_outer_iteration(t, vx, vy, p, dt):
     '''
-    Take one single timestep
+    make sequential under-relaxation (p. 118)
+    setup p.178 and p. 188
+    1. use last un and pn as starting point for un+1, pn+1
+    2. solve linearized eq. for momentum to get um*
+    3. solve pressure eq. to get p'
+    4. correct velocity um to satisfy cont. and the new pressure pm
+    5. go back to 2. and do it all again (outer iteration)
+    6. when converged to go next timestep
     '''
+    #setup eq. for momentum
+    AE, AN, AW, AS, AP, QP = get_equations_momentum(vx, vy, p, dt)
+    #solve inner iteration for u
+    solver_inner_iteration()
+    #setup eq. for pressure
+    get_equations_pressure()
+    #sove inner iteration
+    solver_inner_iteration()
+    #correct velocity and pressure
+    correct_presure_velocity()
+    return None
 
-    return 0
 
-def solver_derivatives():
+def solver_inner_iteration():
     '''
-    Determine derivatives
+    make iterative solution (p. 100)
     '''
+    return None
 
+
+def get_equations_momentum(vx, vy, p, dt):
+    '''
+    make system of equations to solve
+    Ap*up + AL*ul = Qp (eq. 7.97) 
+    '''
+    return None, None, None, None, None, None
+
+def get_equations_pressure():
+    '''
+    make system of equations to solve
+    Ap*p'p + AL*p'l = dm (eq. 7.111) 
+    '''
+    return None
+
+def correct_presure_velocity():
+    '''
+    eq. 7.107??
+    '''
     return None
